072fe2edbafae747e0e8da4adbc247f5adec578e
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-09-26 11:24:18 +0200

    added sponsor seeder

---
+++ config/sponsors.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    [
+        "name" => "Sponsorizzazione 24h",
+        "duration" => "24:00:00",
+        "price" => 2.99,
+    ],
+    [
+        "name" => "Sponsorizzazione 72h",
+        "duration" => "72:00:00",
+        "price" => 5.99,
+    ],
+    [
+        "name" => "Sponsorizzazione 144h",
+        "duration" => "144:00:00",
+        "price" => 9.99,
+    ],
+];
