b105471df4c35087bf35386946b80074f4ad450c
TALEX1995 <tanase.alex@libero.it>
2023-09-27 12:54:04 +0200

    Added store house controller

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use App\Models\House;
 use App\Models\Service;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class HouseController extends Controller
 {
@@ -34,7 +38,42 @@ class HouseController extends Controller
      */
     public function store(Request $request)
     {
-        return to_route("admin.houses.show");
+        // Take data
+        $data = $request->all();
+
+        // New House
+        $house = new House();
+
+        // Add User id in house
+        $house->user_id = Auth::id();
+
+        // Add Address
+        $address = new Address();
+        $address->home_address = $data['home_address'];
+        $address->latitude = $data['latitude'];
+        $address->longitude = $data['longitude'];
+        $address->save();
+        $house->address_id = $address->id;
+
+        // Add Photo in house
+        if (array_key_exists('photo', $data)) {
+            $photo_path = Storage::putFile('house_img', $data['photo']);
+            $data['photo'] = $photo_path;
+        }
+
+        // Fill House
+        $house->fill($data);
+
+        // Save house into db
+        $house->save();
+
+        // Add relation many to many with service
+        if (array_key_exists('service', $data)) {
+            $house->services()->attach($data['service']);
+        }
+
+
+        // return to_route("admin.houses.create");
     }
 
     /**
--- resources/views/includes/form.blade.php
+++ resources/views/includes/form.blade.php
@@ -1,8 +1,9 @@
 <div class="container">
   <h1 class="my-5 text-center">Inserisci i campi richiesti</h1>
-  <form action="{{ route('user.houses.store') }}" method="POST">
+  <form action="{{ route('user.houses.store') }}" method="POST" id="form-houses" enctype="multipart/form-data">
     @csrf
     <div class="row">
+      {{-- Name --}}
       <div class="col-6">
         <div class="mb-3">
           <label for="name" class="form-label">Nome struttura</label>
@@ -10,6 +11,7 @@
             placeholder="Inserisci il nome della casa">
         </div>
       </div>
+      {{-- Structure type --}}
       <div class="col-6">
         <div class="mb-3">
           <label for="type" class="form-label">Tipo di struttura</label>
@@ -21,60 +23,79 @@
           </select>
         </div>
       </div>
+      {{-- Address --}}
+      <div class="col-12">
+        <div class="mb-3">
+          <label for="address" class="form-label">Indirizzo</label>
+          <input type="text" class="form-control" id="address" name="home_address"
+            placeholder="Es. Via Francesco Benaglia, 13, 00153 Roma">
+        </div>
+      </div>
+      <input class="d-none" type="text" id="latitude" name="latitude">
+      <input class="d-none" type="text" id="longitude" name="longitude">
+      {{-- Price per night --}}
       <div class="col">
         <div class="mb-3">
           <label for="price" class="form-label">Prezzo per notte</label>
-          <input type="number" class="form-control" id="price" name="night_price">
+          <input type="number" step="0.01" class="form-control" id="price" name="night_price">
         </div>
       </div>
+      {{-- Bath numb --}}
       <div class="col">
         <div class="mb-3">
           <label for="bath" class="form-label">Numero bagni</label>
           <input type="number" class="form-control" id="bath" name="total_bath">
         </div>
       </div>
+      {{-- Rooms numb --}}
       <div class="col">
         <div class="mb-3">
           <label for="rooms" class="form-label">Numero camere</label>
           <input type="number" class="form-control" id="rooms" name="total_rooms">
         </div>
       </div>
+      {{-- Beds Numb --}}
       <div class="col">
         <div class="mb-3">
           <label for="beds" class="form-label">Numero posti letto</label>
           <input type="number" class="form-control" id="beds" name="total_beds">
         </div>
       </div>
+      {{-- MQ --}}
       <div class="col">
         <div class="mb-3">
           <label for="mq" class="form-label">Metri quadri</label>
-          <input type="number" class="form-control" id="mq" name="total_mq">
+          <input type="number" class="form-control" id="mq" name="mq">
         </div>
       </div>
+      {{-- Description --}}
       <div class="col-12">
         <div class="mb-3">
           <label for="description" class="form-label">Descrizione della struttura</label>
-          <textarea class="form-control" id="description" rows="5"></textarea>
+          <textarea class="form-control" id="description" name="description" rows="5"></textarea>
         </div>
       </div>
+      {{-- Photo --}}
       <div class="col-6">
         <div class="mb-3">
           <label for="photo" class="form-label">Inserisci l'immagine della casa</label>
           <input class="form-control" type="file" id="photo" name="photo">
         </div>
       </div>
+      {{-- Is Publish --}}
       <div class="col-12 mt-3">
         <div class="form-check form-switch">
           <label class="form-check-label" for="published">Pubblica</label>
           <input class="form-check-input" type="checkbox" role="switch" id="published">
         </div>
       </div>
+      {{-- Services --}}
       <h2 class="text-center">Servizi aggiuntivi</h2>
       <div class="col-12 d-flex gap-3 flex-wrap mt-4">
         @foreach ($services as $service)
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="{{ $service->id }}" id="service-{{ $service->id }}"
-              name="service[]">
+            <input class="form-check-input" type="checkbox" value="{{ $service->id }}"
+              id="service-{{ $service->id }}" name="service[]">
             <label class="form-check-label" for="service-{{ $service->id }}">
               {{ $service->name }}
             </label>
@@ -88,3 +109,6 @@
     </div>
   </form>
 </div>
+
+
+@vite(['resources/js/scriptAddress.js'])
