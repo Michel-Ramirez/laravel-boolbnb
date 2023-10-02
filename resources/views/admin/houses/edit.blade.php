bb6bc84732162367bbd88ed924c46eb122b741c1
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-10-02 12:30:18 +0200

    added validation house form

--- resources/views/admin/houses/edit.blade.php
+++ resources/views/admin/houses/edit.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="container">
         <h1 class="my-5 text-center">Modifica i dati della tua casa</h1>
-        <form action="{{ route('user.houses.update', $house) }}" method="POST" id="form-houses" enctype="multipart/form-data">
+        <form action="{{ route('user.houses.update', $house) }}" method="POST" id="form-houses"
+            enctype="multipart/form-data"data-type="edit">
             @method('PUT')
             @include('includes.form')
         </form>
