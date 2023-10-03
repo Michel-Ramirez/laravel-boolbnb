c0ffb141dc93a271cf76ca8658b16009cd703f8d
Mario-Marchio <mariomarchio1610@gmail.com>
2023-10-03 12:23:35 +0200

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+        integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=='
+        crossorigin='anonymous' />
+    @vite('resources\scss\404.scss')
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="error-404">
+        <div class="error-content">
+            <strong>Errore 4<i class="fa-solid fa-house-circle-exclamation fa-shake fa-2x"></i>4</strong>
+            <p>Pagina non trovata</p>
+        </div>
+    </div>
+</body>
+
+</html>
