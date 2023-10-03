332d82a2752663a436d3d11da60feeab5ce4a1ea
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-10-03 19:10:22 +0200

    added api for searchBar

--- app/Http/Controllers/Api/HouseController.php
+++ app/Http/Controllers/Api/HouseController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\House;
 use DateTime;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HouseController extends Controller
 {
@@ -63,5 +65,63 @@ class HouseController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    /**
+     * Api for Searchbar
+     */
+    public function search(Request $request)
+    {
+        $houses = House::with("address")->where("is_published", "1")->get();
+        $num = (int)$request->distance;
+        $housesList = [];
+        foreach ($houses as $house) {
+            $houseList =
+                [
+                    "address" => [
+                        "freeformAddress" => $house->name,
+                        "idHouse" => $house->id,
+                    ],
+                    "position" => [
+                        "lat" => $house->address->latitude,
+                        "lon" => $house->address->longitude,
+                    ]
+                ];
+            $housesList[] = $houseList;
+        };
+        $data =
+            [
+                "geometryList" => [
+                    [
+                        "position" => "$request->lat , $request->long",
+                        "radius" => $num,
+                        "type" => "CIRCLE",
+                    ]
+                ],
+                "poiList" => $housesList,
+            ];
+
+        $client = Http::withOptions([
+            'verify' => false,
+        ]);
+
+        $response = $client->post(
+            "https://api.tomtom.com/search/2/geometryFilter.json?key=soH7vSRFYTpCT37GOm8wEimPoDyc3GMe",
+            $data
+        );
+
+        if ($response->successful()) {
+            // La chiamata API è andata a buon fine.
+            $responseData = $response->json();
+            $housesList = [];
+            foreach ($responseData["results"] as $result) {
+                $house = House::with("address", "sponsors")->where("is_published", "1")->where("id", $result["address"]["idHouse"])->first();
+                $housesList[] = $house;
+            }
+            return response()->json($housesList);
+            // Puoi elaborare la risposta qui.
+        } else {
+            return response()->json("La chiamata API non è andata a buon fine.", 500);
+        }
     }
 }
