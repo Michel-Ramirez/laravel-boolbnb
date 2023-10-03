e590bd1ca8e71c3f6d6c8443678c206130d6ca47
Mario-Marchio <mariomarchio1610@gmail.com>
2023-10-03 12:26:52 +0200

    miro fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,6 +9,8 @@
         integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=='
         crossorigin='anonymous' />
     @vite('resources\scss\404.scss')
+    @vite(['resources/js/app.js'])
+
     <title>Document</title>
 </head>
 
@@ -17,6 +19,7 @@
         <div class="error-content">
             <strong>Errore 4<i class="fa-solid fa-house-circle-exclamation fa-shake fa-2x"></i>4</strong>
             <p>Pagina non trovata</p>
+            <a href="{{ route('user.houses.index') }}" class="btn btn-secondary">Torna alla Tabella</a>
         </div>
     </div>
 </body>
