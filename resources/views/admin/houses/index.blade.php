42f2c870666a988114959655b322bb2552d740fd
TALEX1995 <tanase.alex@libero.it>
2023-10-08 17:52:33 +0200

    Added card in index houses

--- resources/views/admin/houses/index.blade.php
+++ resources/views/admin/houses/index.blade.php
@@ -2,70 +2,8 @@
 
 @section('content')
   <div class="container mt-5">
-    <div class="d-flex justify-content-end mb-3"><a href="{{ route('user.houses.create') }}" class="btn btn-primary">Aggiungi
+    <div class="d-flex justify-content-end mb-4"><a href="{{ route('user.houses.create') }}" class="btn btn-primary">Aggiungi
         Casa</a></div>
-    <div class="table-responsive">
-
-
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Nome Casa</th>
-            <th scope="col">Tipo</th>
-            <th scope="col">Prezzo per notte</th>
-            <th scope="col">Metri quadri</th>
-            <th scope="col">Stato</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          @forelse ($houses as $house)
-            <tr class="align-middle">
-              <th scope="row">{{ $house->name }}</th>
-              <td>{{ $house->type }}</td>
-              <td>{{ $house->night_price }}€</td>
-              <td>
-                @if ($house->mq)
-                  {{ $house->mq }} mq
-                @else
-                  ND
-                @endif
-              </td>
-              <td>
-                <form action="{{ route('user.houses.publish', $house) }}" id="is-published-form" method="POST">
-                  @csrf
-                  @method('PATCH')
-                  <div class="form-check form-switch ms-2">
-                    <input class="form-check-input" type="checkbox" role="switch" id="is_published-{{ $house->id }}"
-                      onclick="form.submit()" name="is_published" @if ($house->is_published == true) checked @endif>
-                    <label class="form-check-label" for="is_published-{{ $house->id }}">
-                      {{ $house->is_published ? 'Visibile' : 'Non Visibile' }}
-                    </label>
-                  </div>
-                </form>
-              </td>
-              <td>
-                <div class="d-flex justify-content-end align-items-center gap-2">
-                  <a href="{{ route('user.houses.edit', $house) }}" class="btn btn-warning">Modifica</a>
-                  <a href="{{ route('user.houses.show', $house) }}" class="btn btn-info">Dettaglio</a>
-                  <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                    data-bs-target="#modal-{{ $house->id }}">
-                    Elimina
-                  </button>
-                  @include('includes.trash.modal')
-                </div>
-              </td>
-            </tr>
-          @empty
-            <td colspan="5">
-              <h3>Non ci sono case</h3>
-            </td>
-          @endforelse
-        </tbody>
-      </table>
-    </div>
-  </div>
-  <div class="container mt-5">
     @foreach ($houses as $house)
       <div class="card mb-3">
         <div class="row g-0">
@@ -76,9 +14,13 @@
           </div>
           <div class="col-md-8">
             <div class="card-body">
+
               <div class="d-flex justify-content-between">
                 <h5 class="card-title">{{ $house->name }}</h5>
-                <h5>Prezzo per Notte: {{ $house->night_price }}€</h5>
+                <div>
+                  <h5>Prezzo per Notte: {{ $house->night_price }}€</h5>
+                  <p><small>{{ $house->address->home_address }}</small></p>
+                </div>
               </div>
               <p class="card-text">{{ $house->description }}</p>
               <div>
@@ -87,6 +29,7 @@
                     bagni: {{ $house->total_bath }} | stanze: {{ $house->total_rooms }} |
                     letti: {{ $house->total_beds }}</small></p>
               </div>
+
               <div class="d-flex align-items-center justify-content-between mt-3">
                 <form action="{{ route('user.houses.publish', $house) }}" id="is-published-form" method="POST">
                   @csrf
