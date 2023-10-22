87ab9c0515ae2e7d27e0dbaf8b5a450910aa0598
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-10-22 19:17:07 +0200

    fix index

--- resources/views/admin/houses/index.blade.php
+++ resources/views/admin/houses/index.blade.php
@@ -12,12 +12,14 @@
         @forelse($houses as $house)
             <div class="card mb-3">
                 <div class="row g-0">
-                    @foreach ($house->photos as $photo)
-                        <div class="col-md-4 col-img">
-                            <img src="{{ $photo->img ? asset('storage/' . $photo->img) : 'https://saterdesign.com/cdn/shop/products/property-placeholder_a9ec7710-1f1e-4654-9893-28c34e3b6399_600x.jpg?v=1500393334' }}"
-                                class="rounded-start  img-fluid">
-                        </div>
-                    @endforeach
+                    <div class="col-md-4 col-img">
+                        @foreach ($house->photos as $photo)
+                            @if ($loop->first)
+                                <img src="{{ $photo->img ? asset('storage/' . $photo->img) : 'https://saterdesign.com/cdn/shop/products/property-placeholder_a9ec7710-1f1e-4654-9893-28c34e3b6399_600x.jpg?v=1500393334' }}"
+                                    class="rounded-start  img-fluid">
+                            @endif
+                        @endforeach
+                    </div>
                     <div class="col-md-8">
                         <div class="card-body pb-0">
                             <div class="d-flex justify-content-between">
